f2ceba0fa0722412cbff8a3208fb8949c1e84494
Pedro Moraes <pm-rogerio@hotmail.com>
2025-06-18 15:03:46 -0300

    Commit do projeto

--- scripts/fusao_mercado_fev.py
+++ scripts/fusao_mercado_fev.py
@@ -1,68 +1,21 @@
-import json
-import csv
+# Importação
+from processamento_dados import Dados
 
-# Criando as funções
-def leitura_json(path_json):
-    dados_json = []
-    with open(path_json, 'r') as file:
-        dados_json = json.load(file)
-    return dados_json    
-
-def leitura_csv(path_csv):
-    dados_csv = []
-    with open(path_csv, 'r') as file:
-        spamreader = csv.DictReader(file, delimiter=',')
-        for row in spamreader:
-            dados_csv.append(row)
-    return dados_csv
-
-def leitura_dados(path, tipo_arquivo):
-    dados = []
-    if tipo_arquivo == 'csv':
-        dados = leitura_csv(path)
-    elif tipo_arquivo == 'json':
-        dados = leitura_json(path)
-    return dados
-
-def get_columns(dados):
-    return list(dados[0].keys())
-
-def rename_columns(dados, key_mapping):
-    new_dados_csv = []
-    for old_dict in dados:
-        dict_temp = {}
-        for old_key, value in old_dict.items():
-            dict_temp[key_mapping[old_key]] = value
-        new_dados_csv.append(dict_temp)
-    
-    return new_dados_csv
-        
-def size_data(dados):
-    return len(dados)
-
-def join(dadosA, dadosB):
-    combined_list = []
-    combined_list.extend(dadosA)
-    combined_list.extend(dadosB)
-    return combined_list   
-
+# Caminhos
 path_json = 'data_raw/dados_empresaA.json'
 path_csv = 'data_raw/dados_empresaB.csv'
+path_dados_combinados = 'data_processed/dados_combinados.csv'
 
-# Iniciando a leitura
-dados_json = leitura_dados(path_json, 'json')
-nome_colunas_json = get_columns(dados_json)
-tamanho_dados_json = size_data(dados_json)
-print(nome_colunas_json)
-print(tamanho_dados_json)
+# Extract
+dados_empresaA = Dados(path_json, 'json')
+print(f'Nome das colunas a empresa A: {dados_empresaA.nome_colunas}')
+print(f'Quantidade de linhas da empresa A: {dados_empresaA.qtd_linhas}\n')
 
-dados_csv = leitura_dados(path_csv, 'csv')
-nome_colunas_csv = get_columns(dados_csv)
-tamanho_dados_csv = size_data(dados_csv)
-print(nome_colunas_csv)
-print(tamanho_dados_csv)
+dados_empresaB = Dados(path_csv, 'csv')
+print(f'Nome das colunas a empresa B: {dados_empresaB.nome_colunas}')
+print(f'Quantidade de linhas da empresa B: {dados_empresaB.qtd_linhas}\n')
 
-# Transformação dos dados
+# Tranform
 key_mapping = {'Nome do Item': 'Nome do Produto',
                 'Classificação do Produto': 'Categoria do Produto',
                 'Valor em Reais (R$)': 'Preço do Produto (R$)',
@@ -70,12 +23,13 @@ key_mapping = {'Nome do Item': 'Nome do Produto',
                 'Nome da Loja': 'Filial',
                 'Data da Venda': 'Data da Venda'}
 
-dados_csv = rename_columns(dados_csv, key_mapping)
-nome_colunas_csv = get_columns(dados_csv)
-print(nome_colunas_csv)
+dados_empresaB.rename_columns(key_mapping)
+print(f'Nome das colunas da empresa B atualizado: {dados_empresaB.nome_colunas}\n')
 
-dados_fusao = join(dados_json, dados_csv)
-nome_colunas_fusao = get_columns(dados_fusao)
-tamanho_dados_fusao = size_data(dados_fusao)
-print(nome_colunas_fusao)
-print(tamanho_dados_fusao)
+dados_fusao = Dados.join(dados_empresaA, dados_empresaB)
+print(f'Nome das colunas das duas empresas juntas: {dados_fusao.nome_colunas}')
+print(f'Nova quantidade de linhas: {dados_fusao.qtd_linhas}')
+
+# Load
+dados_fusao.salvando_dados(path_dados_combinados)
+print(f'Caminho dos dados salvos: {path_dados_combinados}')
